ae5e5974357571a28cda4d292eac83209b19c479
Carlos romero <soportecarlosromero@gmail.com>
2025-08-12 20:46:43 -0500

    initial commit after initializer

---
+++ src/main/java/com/jromax/bcisecurityuserservice/BciSecurityUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jromax.bcisecurityuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BciSecurityUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BciSecurityUserServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jromax/bcisecurityuserservice/BciSecurityUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jromax.bcisecurityuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BciSecurityUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
